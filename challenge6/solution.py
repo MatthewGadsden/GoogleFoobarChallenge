d30588d6c4667948ba1165676e2cedc375212288
MatthewGadsden <matthewjgadsden+coding@gmail.com>
2021-06-29 13:16:24 +1000

    different solution attempts

--- challenge6/solution.py
+++ challenge6/solution.py
@@ -1,3 +1,5 @@
+import math
+
 def solution(i):
     i = int(i)
     bin = "{0:b}".format(i)
@@ -8,19 +10,27 @@ def solution(i):
     c = len(bin) - len(bin.rstrip('0'))
     return c + solution(int(bin.rstrip('0'), 2))
 
+
 def solution2(i):
     i = int(i)
     bin = "{0:b}".format(i)
     count = 0
+    i_count = 0
     if i == 1:
         return 0
+    if len(bin.rstrip('1')) == 0:
+        count -= 1
+    prev = '0'
     for i in bin[1:]:
         if i == '1':
+            if prev == '1':
+                i_count += 1
             count += 2
         if i == '0':
             count += 1
-    return count - 1
+        prev = i
+    return count - (math.ceil(i_count/2)+1)
+
 
 if __name__ == '__main__':
-    [print(i, ': ', solution(i), solution2(i), ',', "{0:b}".format(i)) for i in range(10,21)]
-    print(solution('30001923020901'))
+    [print(i, ': ', solution(i), solution2(i), ',', "{0:b}".format(i)) for i in range(1, 16)]
