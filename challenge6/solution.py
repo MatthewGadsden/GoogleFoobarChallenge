f11f1ce84a5b9f9998c9ce7e96a8baf2a75d4cdd
MatthewGadsden <matthewjgadsden+coding@gmail.com>
2021-06-29 14:09:30 +1000

    solution6 very close

--- challenge6/solution.py
+++ challenge6/solution.py
@@ -10,27 +10,32 @@ def solution(i):
     c = len(bin) - len(bin.rstrip('0'))
     return c + solution(int(bin.rstrip('0'), 2))
 
+def get_zero_count(bin_n: str):
+    return len(bin_n) - len(bin_n.rstrip('0'))
 
 def solution2(i):
     i = int(i)
-    bin = "{0:b}".format(i)
-    count = 0
-    i_count = 0
     if i == 1:
         return 0
-    if len(bin.rstrip('1')) == 0:
-        count -= 1
-    prev = '0'
-    for i in bin[1:]:
-        if i == '1':
-            if prev == '1':
-                i_count += 1
-            count += 2
-        if i == '0':
-            count += 1
-        prev = i
-    return count - (math.ceil(i_count/2)+1)
-
+    if i == 2:
+        return 1
+    bin = "{0:b}".format(i)
+    count = get_zero_count(bin)
+    if count == len(bin) - 1:
+        return count
+    new_bin = bin.rstrip('0')
+    new_n = int(new_bin, 2)
+    higher = "{0:b}".format(new_n+1)
+    lower = "{0:b}".format(new_n-1)
+    if len(higher) > len(lower):
+        return 1 + solution2(int(lower, 2)) + count
+    elif get_zero_count(higher) > get_zero_count(lower):
+        return 1 + solution2(int(higher, 2)) + count
+    else:
+        return 1 + solution2(int(lower, 2)) + count
 
 if __name__ == '__main__':
-    [print(i, ': ', solution(i), solution2(i), ',', "{0:b}".format(i)) for i in range(1, 16)]
+    for i in range(1,129):
+        s1, s2 = solution2(i), solution(i)
+        print(i, ': ', s1, s2) if s1 != s2 else 'pass'
+    # print(solution2(15))
