025eb72eb66d0aa8a0630c07e883fdb0f052a5df
MatthewGadsden <matthewjgadsden+coding@gmail.com>
2021-06-30 13:15:23 +1000

    changed solution implementation to use loop instead of recursion

--- challenge6/solution.py
+++ challenge6/solution.py
@@ -1,11 +1,9 @@
 import math
+from functools import lru_cache
+import time
 
 
-def get_zero_count(bin_n: str):
-    return len(bin_n) - len(bin_n.rstrip('0'))
-
-
-def solution(i):
+def solution_with_recursion(i):
     i = int(i)
     if i == 1:
         return 0
@@ -21,9 +19,8 @@ def solution(i):
         new_bin = bin.rstrip('0')
         new_n = int(new_bin, 2)
         return count + solution(new_n)
-    else:
-        higher = "{0:b}".format(i+1)
-        lower = "{0:b}".format(i-1)
+    higher = "{0:b}".format(i+1)
+    lower = "{0:b}".format(i-1)
     if len(higher) > len(lower):
         return solution(int(higher, 2)) + count + 1
     elif get_zero_count(higher) > get_zero_count(lower):
@@ -32,7 +29,45 @@ def solution(i):
         return 1 + solution(int(lower, 2)) + count
 
 
+def get_zero_count(bin_n: str):
+    return len(bin_n) - len(bin_n.rstrip('0'))
+
+
+def solution(n):
+    total_ops = 0
+    current_n = int(n)
+    while current_n > 0:
+        if current_n == 1:
+            current_n -= 1
+        elif current_n == 2:
+            total_ops += 1
+            current_n -= 2
+        elif current_n == 3:
+            total_ops += 2
+            current_n -= 3
+        else:
+            bin_n = "{0:b}".format(current_n)
+            count = get_zero_count(bin_n)
+            if count == len(bin_n) - 1:
+                total_ops += count
+                current_n -= current_n
+            elif count > 0:
+                total_ops += count
+                new_bin = bin_n.rstrip('0')
+                current_n = int(new_bin, 2)
+            else:
+                higher = "{0:b}".format(current_n + 1)
+                lower = "{0:b}".format(current_n-1)
+                total_ops += (count + 1)
+                if len(higher) > len(lower) or get_zero_count(higher) > get_zero_count(lower):
+                    current_n = int(higher, 2)
+                else:
+                    current_n = int(lower, 2)
+    return total_ops
+
+
 if __name__ == '__main__':
-    n = '3298623563104621673217321730217310732639856102810983219332130921730921723021703212758174298384649835084374874132809309875868356749387504850935840392367213021631936276231908217382613062032109387594376239837219730219732163021163218362362103621063210732630212109372097321097321021313121363026103621063265921321'
-    print(len(n))
+    n = '218632278537539213218729837213213721732183932109832091809375843757754478098084385095485492132132153121321321' \
+        '8540438095535443543357430957430978890980984309485094380098654376478236462934783274932646478364982364236984326' \
+        '437846862364378264237'
     print(solution(n))
